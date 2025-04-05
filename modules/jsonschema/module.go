4cf67812e0a1419b24bec886e03731ca8d9631b0
Jonathan Langevin <codingsloth@pm.me>
2025-04-05 11:03:55 -0400

    Adjusting jsonschema for 1.2.0

--- modules/jsonschema/module.go
+++ modules/jsonschema/module.go
@@ -1,7 +1,6 @@
 package jsonschema
 
 import (
-	"context"
 	"github.com/GoCodeAlone/modular"
 )
 
@@ -15,30 +14,8 @@ func NewModule() *Module {
 	}
 }
 
-func (m *Module) RegisterConfig(modular.Application) {
-	// nothing to do
-}
-
-func (m *Module) Init(modular.Application) error {
-	return nil
-}
-
-func (m *Module) Start(context.Context) error {
-	// Nothing special needed for startup
-	return nil
-}
-
-func (m *Module) Stop(context.Context) error {
-	// Nothing special needed for shutdown
-	return nil
-}
-
 func (m *Module) Name() string {
 	return "jsonschema"
-}
-
-func (m *Module) Dependencies() []string {
-	return nil
 }
 
 func (m *Module) ProvidesServices() []modular.ServiceProvider {
